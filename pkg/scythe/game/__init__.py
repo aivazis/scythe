629f701c955fc5edcb1bc7c7143118a6951d3a67
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-12-12 13:12:53 -0800

    scythe.game: published the land resources

---
+++ pkg/scythe/game/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@orthologue.com>
+# (c) 2018-2020 all rights reserved
+
+
+# publish
+from .LandResource import food, metal, oil, wood
+
+
+# end of file
