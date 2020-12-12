0b1aa352f908f8776cea6765c5128f40feb19b99
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-12-12 12:44:15 -0800

    scythe.resources: {resource} became the base class for all game resources; it only owns the choice of representation and the arithmetic implementation

--- pkg/scythe/Resource.py
+++ pkg/scythe/Resource.py
@@ -6,33 +6,12 @@
 
 class Resource(tuple):
     """
-    The game resources
+    The base class for game resources
     """
 
 
-    # access
-    @property
-    def gold(self):
-        # get my gold content
-        return self[0]
-
-    @property
-    def oil(self):
-        # get my gold content
-        return self[1]
-
-    @property
-    def wood(self):
-        # get my gold content
-        return self[2]
-
-    @property
-    def food(self):
-        # get my gold content
-        return self[3]
-
-
-    # arithmetic
+    # arithmetic; there are no in-place versions since tuples are read-only
+    # combine two instance to make a new one
     def __add__(self, other):
         # go through my resources and {other}'s and add them together
         return type(self)(r1+r2 for r1, r2 in zip(self, other))
@@ -41,6 +20,7 @@ class Resource(tuple):
         # subtract {other}'s resources from mine
         return type(self)(r1-r2 for r1, r2 in zip(self, other))
 
+    # scalar multiplication
     def __mul__(self, other):
         # multiply all my resources by {other}
         return type(self)(other*r for r in self)
@@ -48,13 +28,6 @@ class Resource(tuple):
     def __rmul__(self, other):
         # multiply all my resources by {other}
         return type(self)(other*r for r in self)
-
-
-# constants
-gold = Resource((1,0,0,0))
-oil = Resource((0,1,0,0))
-wood = Resource((0,0,1,0))
-food = Resource((0,0,0,1))
 
 
 # end of file
